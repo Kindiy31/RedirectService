f9609bff7a201f306de55939e5a88d98752a00c7
Kindiy31 <71168657+Kindiy31@users.noreply.github.com>
2024-03-22 14:58:10 +0200

    Upload project

--- TG_bot.py
+++ TG_bot.py
@@ -1,6 +1,7 @@
 import telebot
 import config
 from channel_moderator import ChannelModerator
+from logger_config import logger
 
 bot = telebot.TeleBot(config.TOKEN)
 bot.parse_mode = "HTML"
@@ -11,4 +12,8 @@ link = moderator.create_invite_link()
 
 @bot.message_handler(content_types=['new_chat_members'])
 def new_chat_members_handler(message):
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    logger.info(f"Get new message and delete after: {message}")
+    try:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    except Exception as e:
+        logger.error(f"Error delete message: {e}")
