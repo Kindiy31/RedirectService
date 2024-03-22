6bcd32870d0ec4f2c2c3bd0bbf7cc925bc4147c6
Kindiy31 <71168657+Kindiy31@users.noreply.github.com>
2024-03-22 11:16:44 +0200

    Upload project

--- install_python.sh
+++ install_python.sh
@@ -2,12 +2,12 @@
 
 sudo apt update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/3.9.8/Python-3.9.8.tgz
-tar -xf Python-3.9.8.tgz
-cd Python-3.9.8
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz
+tar -xf Python-3.12.0.tgz
+cd Python-3.12.0
 ./configure --enable-optimizations
 make
 sudo make altinstall
 cd /home
-python3.9 -m pip install --upgrade pip
+python3.12 -m pip install --upgrade pip
 pip install virtualenv
