00a104611df2738c9b7c57f28fefbc4e05d2711f
Kindiy31 <71168657+Kindiy31@users.noreply.github.com>
2024-03-22 17:39:01 +0200

    Upload project

--- TG_bot.py
+++ TG_bot.py
@@ -19,6 +19,15 @@ def new_chat_members_handler(message):
         logger.error(f"Error delete message: {e}")
 
 
+@bot.message_handler()
+def other_messages(message):
+    logger.info(f"Get new message and delete after: {message}")
+    try:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    except Exception as e:
+        logger.error(f"Error delete message: {e}")
+
+
 if __name__ == "__main__":
     logger.info(f"Starting bot")
     bot.polling(True)
